9140c379edeb63c57a3f54682bedbb8c026a6d4b
Eric A. Meyer <eric@meyerweb.com>
2015-01-24 22:13:55 -0500

    First commit (beta.01)

---
+++ index.php
@@ -0,0 +1,120 @@
+<?php
+
+$debug = true;
+if ($debug) {
+	error_reporting(E_ALL);
+	ini_set('display_errors', '1');
+	$start = microtime(true);
+}
+
+if (isset($_GET['pc'])) {
+	$pc = $_GET['pc'];
+	$submitted = true;
+} else {
+	$pc = 5;
+	$submitted = false;
+}
+(isset($_GET['pl']) ? $pl = $_GET['pl'] : $pl = 2);
+(isset($_GET['sl']) ? $sl = $_GET['sl'] : $sl = 2);
+(isset($_GET['ln']) ? $ln = $_GET['ln'] : $ln = 2);
+(isset($_GET['lf']) ? $lf = $_GET['lf'] : $lf = '');
+
+include("localipsum.php");
+$array = loadData();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+<title><?php echo $array["title"] ?></title>
+<style type="text/css">
+
+body {padding: 2em;	font: 95% Helvetica, sans-serif;}
+body h1 {margin: 0; padding: 0.5em 0.25em 0;
+	background: rgba(0,0,0,0.25);
+	font-size: 175%;}
+body ol {margin: 0 0 1em; padding: 0;}
+body li {margin-top: 0.5em; list-style: none;}
+
+form {border: 0.25em solid silver;
+	margin-bottom: -0.25em; padding: 1em 2em;
+	background: rgba(0,0,0,0.05);}
+
+.debug {position: absolute; top: 0; right: 0;
+	margin: 0; padding: 0.5em;
+	border: 1px solid rgba(204,0,0,0.5); border-width: 0 0 1px 1px;
+	color: #633; background: rgba(204,0,0,0.05);
+	font-size: 90%;}
+.array-reset {color: maroon; background: yellow;}
+
+.output {border: 0.25em solid gray;
+	padding: 1em 2em;
+	font: medium/1.3 Georgia, serif;}
+
+footer {position: absolute; top: 5em; right: 5em;
+	margin: 0; padding: 0;
+	font-style: italic; font-size: small; text-align: right;
+	opacity: 0.5;}
+
+/*
+ol {display: flex;}
+li {justify-content: stretch; flex-grow: 1;}
+ol label {display: block; background: silver; padding: 0.5em 0.5em 0.25em; margin: 0 0.1em 0.33em;}
+ol + p {text-align: center; margin: 0;}
+ol + p input {font-weight: bold;}
+*/
+</style>
+</head>
+<body>
+
+<h1><?php echo $array["title"] ?></h1>
+
+<form method="get">
+
+<ol>
+<li><label for="pc">How many paragraphs do you want?</label>
+<input type="text" value="<?php echo $pc; ?>" size="3" maxlength="2" name="pc" id="pc"></li>
+<li>How long should paragraphs be?
+<label><input type="radio" name="pl" value="1"<?php if ($pl == 1) echo " checked" ?>>Short</label>
+<label><input type="radio" name="pl" value="2"<?php if ($pl == 2) echo " checked" ?>>Moderate</label>
+<label><input type="radio" name="pl" value="3"<?php if ($pl == 3) echo " checked" ?>>Long</label>
+<label><input type="radio" name="pl" value="4"<?php if ($pl == 4) echo " checked" ?>>Epic</label>
+<label><input type="radio" name="pl" value="5"<?php if ($pl == 5) echo " checked" ?>>Joycean</label>
+</li>
+<li>How long should sentences be?
+<label><input type="radio" name="sl" value="1"<?php if ($sl == 1) echo " checked" ?>>Short</label>
+<label><input type="radio" name="sl" value="2"<?php if ($sl == 2) echo " checked" ?>>Moderate</label>
+<label><input type="radio" name="sl" value="3"<?php if ($sl == 3) echo " checked" ?>>Long</label>
+<label><input type="radio" name="sl" value="4"<?php if ($sl == 4) echo " checked" ?>>Run-on</label>
+<label><input type="radio" name="sl" value="5"<?php if ($sl == 5) echo " checked" ?>>Faulkneresque</label>
+</li>
+<li>How local do you want to get?
+<label><input type="radio" name="ln" value="0"<?php if ($ln == 0) echo " checked" ?>>All local, all the time!</label>
+<label><input type="radio" name="ln" value="1"<?php if ($ln == 1) echo " checked" ?>>Very local</label>
+<label><input type="radio" name="ln" value="2"<?php if ($ln == 2) echo " checked" ?>>Kind of local</label>
+<label><input type="radio" name="ln" value="3"<?php if ($ln == 3) echo " checked" ?>>Not very local</label>
+<label><input type="radio" name="ln" value="4"<?php if ($ln == 4) echo " checked" ?>>Regional</label>
+<label><input type="radio" name="ln" value="5"<?php if ($ln == 5) echo " checked" ?>>Remote</label>
+</li>
+<!-- li><label>Where’s your city data? (optional)</label> <input type="file" name="cd" id="cd"></li -->
+</ol>
+<p>
+<input type="submit" value="Let’s do this!">
+</p>
+
+</form>
+
+<?php
+
+if ($submitted) makeText($pc, $pl, $sl, $ln, $array);
+
+?>
+
+<footer>
+Local Ipsum (beta.01) • A <a href="http://meyerweb.com/">meyerweb</a> joint
+</footer>
+
+</body>
+</html>
---
+++ localipsum.php
@@ -0,0 +1,116 @@
+<?php
+
+
+function loadData($fname = "local.txt") {
+	$dir = "./";
+
+	$fs=fopen($dir.$fname, "r");
+	while (!feof($fs)) {
+	    $array[] = trim(fgets($fs));
+	}
+	fclose($fs);
+
+	$title = array_splice($array, 0, 1)[0];
+	$locale = array_splice($array, 0, 1)[0];
+
+	for ($a = 0; $a < count($array); $a++) {
+		$string = $array[$a];
+		$cmtp = strpos($string,'//');
+		if ($cmtp !== false) {
+			$string = substr($string,0,$cmtp);
+		}
+		if ($string == '') {
+			array_splice($array, $a, 1);
+			$a--;
+		}
+	}
+
+	return array("title" => $title, "locale" => $locale, "local" => $array, "ipsum" => "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.");
+
+}
+
+
+function makeText($pc = 5, $pl = 2, $sl = 2, $ln = 2, $data) {
+	global $debug, $start;  // yeah, yeah
+
+	$array = $data["local"];
+	$ipsum_array = explode(" ", preg_replace("/[.,;]/","",$data["ipsum"]));
+
+	while (count($ipsum_array) < count($array)) {
+		$ipsum_array = array_merge($ipsum_array,$ipsum_array);
+	}
+	array_splice($ipsum_array,count($array));
+
+	if ($debug) {
+		echo "<p class=\"debug\"><a href=\"local.txt\">Input</a>: ";
+		echo count($array);
+		echo " • Filler: ";
+		echo count($ipsum_array);
+	}
+
+//	$mc = 1;	// 0 = no filler; 5 = almost all filler; 10 = WTF
+	for ($m = 0; $m < $ln; $m++) {
+		$array = array_merge($array, $ipsum_array);
+	}
+
+	if ($debug) {
+		echo " • Source: ";
+		echo count($array);
+	}
+
+	shuffle($array);
+	reset($array);
+
+	$output = '';
+
+//	$pc = rand(4,7);
+	
+	for ($p = 0; $p < $pc; $p++) {
+		$output .= "<p>";
+		$sc = rand($pl,$pl*2)*3;
+		for ($s = 0; $s < $sc; $s++) {
+			$wc = rand($sl,$sl*3)*2;
+			for ($w = 0; $w < $wc; $w++) {
+				if (!current($array)) {
+					shuffle($array);
+					reset($array);
+					if ($debug) {
+						echo " <span class=\"array-reset\">↺</span>";
+						$output .= " <b class=\"array-reset\">↺</b> ";
+					}
+				}
+				$word = strtolower(trim(current($array)));
+				if ($w == 0) $word = ucfirst($word);
+				if ($w > $wc/4 && $w < $wc*3/4) {
+					$cp = 8;
+					if (!(rand(0,$cp))) $word .= ",";
+				}
+				$output .= $word . " ";
+				next($array);	
+			}
+		$output = rtrim($output) . ". ";
+		}
+	$output = rtrim($output) . "</p>\n";
+	}
+
+	if ($debug) {
+		$end = microtime(true);
+		$creationtime = ($end - $start);
+		printf(" • %.5f sec", $creationtime);
+		echo "</p>\n\n";
+	}
+
+	echo "<div class=\"output\">\n";
+
+	echo $output;
+	
+	echo "</div>\n";
+
+	if ($debug) {
+		echo "<pre>" . var_dump($array) . "</pre>";
+	}
+
+}
+
+
+?>
